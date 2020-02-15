4e8bcd8a281bd52467ea1101b333448d8627f388
keithwang <363852386@qq.com>
2020-02-15 19:05:53 +0800

    ‘重置默认样式路由Element’

---
+++ src/views/Login/index.vue
@@ -0,0 +1,17 @@
+<template>
+    <div id="login"> hello 你好呀<el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button></div>
+</template>
+<script>
+export default {
+    name:"login"
+}
+</script>
+
+<style lang="scss" scoped>
+    #login{
+        height: 50vh;
+        background-color: yellow
+    }
+
+</style>
